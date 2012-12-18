7d88490eabee45de55ff929b3eb052df64194a6a
yewton <yewton@gmail.com>
2012-12-18 22:22:14 +0900

    fix typo

--- sudden-death.el
+++ sudden-death.el
@@ -11,7 +11,7 @@
 ;; Enclose the active region with a fancy fence.
 ;; cf. https://github.com/aquarla/sudden_death
 
-;; To install, save this on your load path and add the fallowing to
+;; To install, save this on your load path and add the following to
 ;; your .emacs file:
 ;;
 ;; (require 'sudden-death)
